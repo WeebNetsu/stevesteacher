e0bb7b3601380601a1d509c660f4c722dad2fb64
WeebNetsu <stephenvdw7777@gmail.com>
2023-11-02 16:45:50 +0200

    Remove src directory

---
+++ app/page.tsx
@@ -0,0 +1,21 @@
+import styles from "./page.module.css";
+
+export default function Home() {
+	return (
+		<main className={styles.main}>
+			<div>
+				<h1>Steve&apos;s teacher</h1>
+				<p>
+					<a target="_blank" href="https://www.youtube.com/@stevesteacher">
+						Learn To Code
+					</a>
+				</p>
+			</div>
+
+			<p>
+				Psst! Website is still in development, come back later for some cool
+				stuff!
+			</p>
+		</main>
+	);
+}
